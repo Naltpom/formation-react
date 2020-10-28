0f10808e1c049c19da66439fd23c48eceab348d7
Nathan <nathan.provost78990@gmail.com>
2020-10-28 16:12:09 +0100

    Formation-React-001
    useEffect
    - get api articles list
    - resize

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import List from '../List/List'
+import Resize from '../Resize/Resize'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <List />
+      <Resize />
     </div>
   );
 }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,31 +1,16 @@
+import { useState, useEffect } from 'react';
 import Article from '../Article/Article'
 import CarItem from '../CarItem/CarItem'
 
 function List() {
-    
-  const arts = [
-        {
-            "id": 1,
-            "title": "Article 1",
-            "category": "News",
-            "published": true,
-            "content": "Lorem ipsum delor sit amet.",
-        },
-        {
-            "id": 2,
-            "title": "Article 2",
-            "category": "News",
-            "published": false,
-            "content": "Lorem ipsum delor sit amet.",
-        },
-        {
-            "id": 3,
-            "title": "Article 3",
-            "category": "Blog",
-            "published": true,
-            "content": "Lorem ipsum delor sit amet.",
-        },
-    ];
+
+    const [articles, setArticles] = useState([]);
+    const url = 'http://localhost:3001/articles';
+    useEffect(() => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setArticles(data))
+    }, [url, setArticles]);
     const products = [
         {
             "id": 1,
@@ -47,8 +32,8 @@ function List() {
     return (
         <div>
             <div className="p-3">Liste des articles</div>
-            <Article article={arts[0]} />
-            {arts.map(art => <Article article={art} foo='bar' key={art.id} />)}
+            <Article article={articles} />
+            {articles.map(art => <Article article={art} foo='bar' key={art.id} />)}
             <div className="p-3">Liste des produits</div>
             {products.map(product => <CarItem produit={product}  key={product.id} />)}
         </div>
